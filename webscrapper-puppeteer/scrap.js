59cb238a9c669135338e2e9928f006224b96bb56
Soto <rodrigo.soto@airesdigital.com.br>
2024-10-15 22:43:59 -0300

    Scraper com rnum tickers

--- webscrapper-puppeteer/scrap.js
+++ webscrapper-puppeteer/scrap.js
@@ -5,70 +5,77 @@ async function main() {
   // Iniciar o navegador e abrir uma nova página
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
+  const startTime = Date.now();
+  let endTime;
 
-try {
-    for (let i = 0; i < Object.keys(tickerEnum).length; i++) {
+    try {
+        for (let i = 0; i < Object.keys(tickerEnum).length; i++) {
 
-        // Navegar até a página alvo
-        await page.goto('https://br.financas.yahoo.com/quote/'+Object.keys(tickerEnum)[i].toString()+'/', {
-          waitUntil: 'networkidle2',
-        });
+            const ticket = Object.values(tickerEnum)[i].toString();
+            // Navegar até a página alvo
+            await page.goto('https://br.financas.yahoo.com/quote/'+ticket+'/', {
+              waitUntil: 'networkidle2',
+            });
 
-        // Capturar os valores desejados
-          const data = await page.evaluate((ticket) => {
-          const selectorPrice = `fin-streamer[data-symbol="${ticket}"][data-field="regularMarketPrice"]`;
-          const selectorPercent = `fin-streamer[data-symbol="${ticket}"][data-field="regularMarketChangePercent"]`;
-          const name = document.querySelector('h1').innerText.substring(0, document.querySelector('h1').innerText.indexOf('(') - 2);
-          const valuePrice = document.querySelector(selectorPrice).innerText; 
-          const valuePercent = document.querySelector(selectorPercent).innerText.trim().replace('(', '').replace(')', '').replace('%', '');
-          
-          const valueOpen = document.querySelector('td[data-test="OPEN-value"]')
-          ? document.querySelector('td[data-test="OPEN-value"]').innerText
-          : null;
-
-          const valueMin = document.querySelector('td[data-test="DAYS_RANGE-value"]')
-              ? document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.substring(0, document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.indexOf(' '))
+            // Capturar os valores desejados
+              const data = await page.evaluate((ticket) => {
+              const selectorPrice = `fin-streamer[data-symbol="${ticket}"][data-field="regularMarketPrice"]`;
+              const selectorPercent = `fin-streamer[data-symbol="${ticket}"][data-field="regularMarketChangePercent"]`;
+              const name = document.querySelector('h1').innerText.substring(0, document.querySelector('h1').innerText.indexOf('(') - 2);
+              const valuePrice = document.querySelector(selectorPrice).innerText; 
+              const valuePercent = document.querySelector(selectorPercent).innerText.trim().replace('(', '').replace(')', '').replace('%', '');
+              
+              const valueOpen = document.querySelector('td[data-test="OPEN-value"]')
+              ? document.querySelector('td[data-test="OPEN-value"]').innerText
               : null;
 
-          const valueMax = document.querySelector('td[data-test="DAYS_RANGE-value"]')
-              ? document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.substring(document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.lastIndexOf(' ') + 1, document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.lenght)
-              : null;
-          
-          const descricaoFechamento = document.querySelector('div[id="quote-market-notice"]')
-              ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(0, document.querySelector('div[id="quote-market-notice"]').innerText.indexOf(':'))
-              : null; 
+              const valueMin = document.querySelector('td[data-test="DAYS_RANGE-value"]')
+                  ? document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.substring(0, document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.indexOf(' '))
+                  : null;
 
-          const dataFechamento = document.querySelector('div[id="quote-market-notice"]')
-              ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(document.querySelector('div[id="quote-market-notice"]').innerText.indexOf(':') + 2, document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') - 3)
-              : null; 
+              const valueMax = document.querySelector('td[data-test="DAYS_RANGE-value"]')
+                  ? document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.substring(document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.lastIndexOf(' ') + 1, document.querySelector('td[data-test="DAYS_RANGE-value"]').innerText.lenght)
+                  : null;
+              
+              const descricaoFechamento = document.querySelector('div[id="quote-market-notice"]')
+                  ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(0, document.querySelector('div[id="quote-market-notice"]').innerText.indexOf(':'))
+                  : null; 
 
-          const horaFechamento = document.querySelector('div[id="quote-market-notice"]')
-              ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') - 2, document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') + 5)
-              : null; 
+              const dataFechamento = document.querySelector('div[id="quote-market-notice"]')
+                  ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(document.querySelector('div[id="quote-market-notice"]').innerText.indexOf(':') + 2, document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') - 3)
+                  : null; 
 
-          return {
-            name,
-            valuePrice,
-            valuePercent,
-            valueOpen,
-            valueMin,
-            valueMax,
-            descricaoFechamento,
-            dataFechamento,
-            horaFechamento,
-          };
-        }, ticket);
+              const horaFechamento = document.querySelector('div[id="quote-market-notice"]')
+                  ? document.querySelector('div[id="quote-market-notice"]').innerText.substring(document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') - 2, document.querySelector('div[id="quote-market-notice"]').innerText.lastIndexOf(':') + 5)
+                  : null; 
 
-        console.log(data);
+              return {
+                name,
+                valuePrice,
+                valuePercent,
+                valueOpen,
+                valueMin,
+                valueMax,
+                descricaoFechamento,
+                dataFechamento,
+                horaFechamento,
+              };
+            }, ticket);
 
+            console.log(data);
+        };
+      } catch (error) {
+        return error;
         // Fechar o navegador
         await browser.close();
-    };
-  } catch (error) {
-    return error;
-  }
+      }
+  // Fechar o navegador
+  await browser.close();
+  endTime = Date.now();
+  const timeTaken = endTime - startTime;
+  console.log(`Tempo total de execução: ${timeTaken/1000} s`);
 }
 
 setInterval(async () => {
     main();
-}, 60000);
+}, 120000);
